d78d6380a3eb08f464b4fe33d3e6f5156b274fda
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-03-19 16:23:39 +0330

    prepare auth service for testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,5 @@
-import express, { json } from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
-const app = express();
-app.set("trust proxy", true);
-
-app.use(json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all("*", async (req, res, next) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 async function start() {
   if (!process.env.JWT_KEY) {
